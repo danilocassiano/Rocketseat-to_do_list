ccdb80c7963fed3f973baeb4a977748afd2934c2
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-10 12:29:42 -0300

    first commit

---
+++ src/test/java/br/com/danilocassiano/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilocassiano.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
