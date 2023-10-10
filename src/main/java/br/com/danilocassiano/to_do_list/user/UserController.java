92294bb0a3264ca86b3dceb4ede39d3a2a5ecafe
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-10 18:49:56 -0300

    Usando o Controller.

---
+++ src/main/java/br/com/danilocassiano/to_do_list/user/UserController.java
@@ -0,0 +1,17 @@
+package br.com.danilocassiano.to_do_list.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
+    }
+}
---
+++ src/main/java/br/com/danilocassiano/to_do_list/user/UserModel.java
@@ -0,0 +1,32 @@
+package br.com.danilocassiano.to_do_list.user;
+
+public class UserModel {
+   private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String name;
+   private String password;
+}
